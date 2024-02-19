accca6d920b273e536ad9a2ec4a76b967d9aeef4
Joserra Gimeno <joserra2304@gmail.com>
2024-02-19 13:54:03 +0100

    first commit

---
+++ src/main/java/com/svalero/glutenvoid/GlutenVoidApiApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.glutenvoid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlutenVoidApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlutenVoidApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svalero/glutenvoid/GlutenVoidApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.glutenvoid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlutenVoidApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
