d2cb35a80a46ae156575e86213d08be8dbd6c09d
Joserra Gimeno <joserra2304@gmail.com>
2024-03-02 15:05:45 +0100

    Cambios DTO

--- src/main/java/com/svalero/glutenvoid/service/UserService.java
+++ src/main/java/com/svalero/glutenvoid/service/UserService.java
@@ -2,11 +2,11 @@ package com.svalero.glutenvoid.service;
 
 import com.svalero.glutenvoid.domain.GlutenCondition;
 import com.svalero.glutenvoid.domain.User;
-import com.svalero.glutenvoid.domain.heritages.UserFavourite;
 
 import java.util.List;
 
 public interface UserService {
+
 
     List<User> findAll();
 
@@ -16,4 +16,6 @@ public interface UserService {
     List<User> filterByGlutenCondition(GlutenCondition glutenCondition);
 
     User addUser(User user);
+
+
 }
