3ef42a1184732bf156d1403236c1c90161a6ab1a
Joserra2304 <joserra2304@gmail.com>
2024-05-25 13:45:45 +0200

    mejoras y despliegue

--- src/main/java/com/svalero/glutenvoid/controller/request/LoginRequest.java
+++ src/main/java/com/svalero/glutenvoid/controller/request/LoginRequest.java
@@ -1,8 +1,18 @@
 package com.svalero.glutenvoid.controller.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class LoginRequest {
     private String username;
     private String password;
+
+    @JsonCreator
+    public LoginRequest(@JsonProperty("username") String username,
+                        @JsonProperty("password") String password) {
+        this.username = username;
+        this.password = password;
+    }
 
     public String getUsername() {
         return username;
