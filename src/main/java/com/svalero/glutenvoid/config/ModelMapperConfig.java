26d4a6f91b3d4a42884d3a9a535698a6976bdc5a
Joserra2304 <joserra2304@gmail.com>
2024-05-26 18:03:40 +0200

    Añadir filtrado en recetas por Id de usuario

--- src/main/java/com/svalero/glutenvoid/config/ModelMapperConfig.java
+++ src/main/java/com/svalero/glutenvoid/config/ModelMapperConfig.java
@@ -1,9 +1,6 @@
 package com.svalero.glutenvoid.config;
 
-import com.svalero.glutenvoid.domain.dto.RecipeDto;
-import com.svalero.glutenvoid.domain.entity.Recipe;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,18 +9,6 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-
-        // Configuración de mapeo personalizado para Recipe a RecipeDto
-        modelMapper.addMappings(new PropertyMap<Recipe, RecipeDto>() {
-            @Override
-            protected void configure() {
-                // Asegúrate de manejar los posibles nulos
-                map().setUserId(source.getUser() != null ? source.getUser().getId() : null);
-                map(source.getUser() != null ? source.getUser().getUsername() : null, destination.getUsername());
-            }
-        });
-
-        return modelMapper;
+        return new ModelMapper();
     }
 }
